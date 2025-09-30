364da6c62918e0e663e0cc80ccc446bc38d7b870
benfeedbag <nizamushahru@gmail.com>
2025-09-30 21:16:33 +0100

    nextconfig

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,6 @@
-import { NextConfig } from "next";
-
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   reactStrictMode: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
